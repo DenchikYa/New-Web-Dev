616fb6675168f86217de9877878217fcf3741d9e
Denchik <v525oa@gmail.com>
2018-02-22 16:49:23 +0400

    Test connection server

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,14 @@
 //Модуль tableApp
-angular.module('tableApp', []).controller('tableController', function tableController($scope) {
+angular.module('tableApp', []).controller('tableController', function tableController($scope, $http) {
     $scope.gamePlace = [];
     var tr1Table = ['X', 'X', null];
     var tr2Table = ['0', null, null];
     var tr3Table = ['0', 'X', null];
+
+    $http.post("http://localhost:8080/api", {slogan:"client"}, {headers:{'Content-Type': 'application/json'}})
+        .then(function (response) {
+            console.log(response);
+    });
 
     $scope.gamePlace.push(tr1Table);
     $scope.gamePlace.push(tr2Table);
